6de68c4e9e29706b0027aa70d9b364e7ee6cc2db
Ross Stapleton <rstapleton@spartaglobal.com>
2019-10-29 15:46:13 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
