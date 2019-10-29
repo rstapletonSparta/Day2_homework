1f68dc0f4d7b9cca48c3746cba8e1996a88d09e2
Ross Stapleton <rstapleton@spartaglobal.com>
2019-10-29 16:33:41 +0000

    add components class sturcture

---
+++ GameComponents.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace GameComponents
+{
+    class Player
+    {
+        private int health;
+
+        private int mana;
+        private int magicDamage;
+
+        private int rageMeter;
+        private int oneHandDamage;
+
+        private int[] position = new int[2];
+
+        public int takeDamage(int damage)
+        {
+            return health - damage;
+        }
+    }
+    class NPC
+    {
+        private int id = 0;
+        private Player player;
+        
+    }
+    class Enemy : NPC
+    {
+        private int health;
+        public int DoDamage(int damage)
+        {
+            return health - damage;
+        }
+    }
+    class Ork : Enemy
+    {
+        private double rageMeter;
+        private double oneHandDamage;
+    }
+    class DarkWizard : Enemy
+    {
+        private double mana;
+        private double magicDamage;
+        
+    }
+    class LightWizard : NPC
+    {
+        private double mana;
+        private double magicDamage;
+    }
+}
