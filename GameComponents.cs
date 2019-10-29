ad295a4d9071a71c896be99a2b0a1fa533e35fb9
Ross Stapleton <rstapleton@spartaglobal.com>
2019-10-29 22:58:06 +0000

    added construtor for player and some attack features for enemy npcs

--- GameComponents.cs
+++ GameComponents.cs
@@ -5,32 +5,56 @@ namespace GameComponents
     class Player
     {
         private int health;
-
         private int mana;
-        private int magicDamage;
-
-        private int rageMeter;
-        private int oneHandDamage;
-
+        private int magicDamage = 10;
+        private int magicRange = 10;
+        private int rageMeter = 0;
+        private int oneHandDamage = 15;
         private int[] position = new int[2];
-
-        public int takeDamage(int damage)
+        
+        public Player(int startHealth, int startMana, int x, int y)
+        {
+            health = startHealth;
+            mana = startMana;
+            position[0] = x;
+            position[1] = y;
+        }
+        // taking damage
+        public int TakeDamage(int damage)
         {
             return health - damage;
         }
+        public int Burn(int burnDamage)
+        {
+            // this should make the player burn for a period
+            return health;
+        }
+
+        // effects from fighting eg less mana or more rage
     }
     class NPC
     {
         private int id = 0;
         private Player player;
-        
+        private int health;
+        private int[] position;
+
+        //public NPC(int npcHealth, int x, int y)
+        //{
+        //    health = npcHealth;
+        //    position[0] = x;
+        //    position[1] = y;
+        //}
+        public int TakeDamage(int damage)
+        {
+            return health - damage;
+        }
     }
     class Enemy : NPC
     {
-        private int health;
-        public int DoDamage(int damage)
+        public void DealDamage(int damage)
         {
-            return health - damage;
+
         }
     }
     class Ork : Enemy
@@ -42,11 +66,12 @@ namespace GameComponents
     {
         private double mana;
         private double magicDamage;
-        
+        // should check range if can melee use magic
     }
     class LightWizard : NPC
     {
         private double mana;
         private double magicDamage;
+        //should be able to heal the player
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using GameComponents;
+
 namespace Day2_homework
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Player player = new Player(200, 100, 10, 0);
+
+            NPC[] npcs =
+            {
+                new DarkWizard(),
+                new DarkWizard(),
+                new Ork(),
+                new Ork()
+            };
         }
     }
 }
+
+
